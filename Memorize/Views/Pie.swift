cb7a91bdff3aded35b2b28f404d792b789761f99
Manuel Lorenzo <manulorenzop@gmail.com>
2021-06-18 14:15:44 +0200

    animate card pie

--- Memorize/Views/Pie.swift
+++ Memorize/Views/Pie.swift
@@ -12,6 +12,16 @@ struct Pie: Shape {
     var endAngle: Angle
     var clockwise = false
     
+    var animatableData: AnimatablePair<Double, Double> {
+        get {
+            AnimatablePair(startAngle.radians, endAngle.radians)
+        }
+        set {
+            startAngle = Angle.radians(newValue.first)
+            endAngle = Angle.radians(newValue.second)
+        }
+    }
+    
     func path(in rect: CGRect) -> Path {
         var p = Path()
         let radius = min(rect.height, rect.width) / 2
